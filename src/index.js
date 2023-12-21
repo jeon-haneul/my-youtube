c81280c6eab6cc24e8bfabe6463406461d721370
jeon-haneul <jeonhaneul97@icolud.com>
2023-12-21 19:29:06 +0900

    2023.12.21 scss 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// style.scss 연동
+import "./assets/scss/style.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
